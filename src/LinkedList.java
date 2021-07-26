aee927aa3a02c9fbfe6e6eb55b43d869e2f3e5f2
bsspencer42 <bsspencer42@gmail.com>
2021-07-26 15:32:22 -0400

    Begin LinkedList class

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,27 @@
-public class LinkedList {
+public class LinkedList<T> implements List<T>{
+    // Instance variables
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    // Constructors
+    public LinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    // Getters
+    public Node<T> getHead(){
+        return this.head;
+    }
+    public Node<T> getTail(){
+        return this.tail;
+    }
+
+    // Overrides
+    @Override
+    public void addAtIndex(T data, int index){
+
+    }
 
 }
